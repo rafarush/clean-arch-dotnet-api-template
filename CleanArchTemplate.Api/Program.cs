83d8104f19fc1ca9a8532905f1eaaa2f1fe29822
rafarush <rgarciagonzalez59@gmail.com>
2025-12-25 04:46:48 -0500

    render deploy

--- CleanArchTemplate.Api/Program.cs
+++ CleanArchTemplate.Api/Program.cs
@@ -92,6 +92,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "CleanArchTemplate API v1");
+        options.RoutePrefix = "swagger";
     });
 }
 
