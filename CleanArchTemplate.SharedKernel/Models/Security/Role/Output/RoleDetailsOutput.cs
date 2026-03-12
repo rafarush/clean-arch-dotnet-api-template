677ac1fcfc154cc4f2bf9e9f6ec4a5757e368391
rafarush <rgarciagonzalez59@gmail.com>
2026-03-12 19:41:11 -0400

    chore: TODO added

--- CleanArchTemplate.SharedKernel/Models/Security/Role/Output/RoleDetailsOutput.cs
+++ CleanArchTemplate.SharedKernel/Models/Security/Role/Output/RoleDetailsOutput.cs
@@ -5,6 +5,7 @@ public class RoleDetailsOutput
     public required Guid Id { get; set; }
     public required string Name { get; set; }
     public required string? Description { get; set; }
+    // TODO refactor List of Policy by a List of the PolicyOutput DTO
     public required List<Domain.Security.Policy> Policies { get; set; }
     public required DateTime CreatedAt { get; set; }
     public required DateTime? UpdatedAt { get; set; }
