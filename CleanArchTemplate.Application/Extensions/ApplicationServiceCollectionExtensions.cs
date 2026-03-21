54a61c1e1deffa94f0042c29024e61ab46eaa3e9
rafarush <rgarciagonzalez59@gmail.com>
2026-03-20 23:13:21 -0400

    build: swagger config moved to ApplicationServiceCollectionExtensions.cs

--- CleanArchTemplate.Application/Extensions/ApplicationServiceCollectionExtensions.cs
+++ CleanArchTemplate.Application/Extensions/ApplicationServiceCollectionExtensions.cs
@@ -5,6 +5,12 @@ using CleanArchTemplate.Application.Abstractions.Cqrs.Query;
 using FluentValidation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using CleanArchTemplate.Infrastructure.Services.Auth.Options;
+using CleanArchTemplate.Application.Features.Auth.Options;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace CleanArchTemplate.Application.Extensions;
 
@@ -18,6 +24,73 @@ public static class ApplicationServiceCollectionExtensions
         services.AddScoped<ICommandSender, CommandSender>();
         services.AddScoped<IQuerySender, QuerySender>();
         
+        return services;
+    }
+
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.Configure<JwtOptions>(config.GetSection("Jwt"));
+
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:SecretKey"]!)),
+                ValidateIssuerSigningKey = true,
+                ValidateLifetime = true,
+                ValidateIssuer = true,
+                ClockSkew = TimeSpan.Zero,
+                ValidIssuer = config["Jwt:Issuer"],
+                ValidateAudience = true,
+                ValidAudience = config["Jwt:Audience"],
+            };
+        });
+
+        services.AddAuthorization();
+        services.AddControllers();
+
+        services.AddSwaggerGen(static x =>
+        {
+            x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                Description = "JWT Authorization header using Bearer scheme"
+            });
+    
+            x.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+    
+            x.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "CleanArchTemplate API",
+                Version = "v1",
+                Description = "Clean Architecture Template and JWT Authentication API"
+            });
+    
+            x.EnableAnnotations();
+        });
+
         return services;
     }
 }
