03586d8e3fae260ba6b5f1e801f9e34cee2989d8
rafarush <rgarciagonzalez59@gmail.com>
2026-03-20 23:31:50 -0400

    build: Use middleware moved to WebApplicationExtensions.cs

--- CleanArchTemplate.Api/Program.cs
+++ CleanArchTemplate.Api/Program.cs
@@ -1,43 +1,24 @@
+using CleanArchTemplate.Application;
 using CleanArchTemplate.Application.Middleware;
 using CleanArchTemplate.Application.Extensions;
 using CleanArchTemplate.Infrastructure.Extensions;
 
 
-
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
-// Add API Services
+
 builder.Services.AddApiServices(config);
 
-// Add Infrastructure Services
 builder.Services.AddInfrastructure(config);
 
-// Add Application Services
 builder.Services.AddApplication(config);
 
-// Add Db
 builder.Services.AddDatabase(config);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.UseMiddleware<ValidatorMapperMiddleware>();
-
-app.MapControllers();
-
-await app.InitializeDatabaseAsync();
+app.UseSharedMiddlewares();
 
 app.Run();
 
---
+++ CleanArchTemplate.Application/WebApplicationExtensions.cs
@@ -0,0 +1,30 @@
+﻿using CleanArchTemplate.Application.Middleware;
+using CleanArchTemplate.Infrastructure.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+namespace CleanArchTemplate.Application;
+
+public static class WebApplicationExtensions
+{
+    public async static void UseSharedMiddlewares(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseMiddleware<ValidatorMapperMiddleware>();
+
+        app.MapControllers();
+        
+        await app.InitializeDatabaseAsync();
+    }
+}
