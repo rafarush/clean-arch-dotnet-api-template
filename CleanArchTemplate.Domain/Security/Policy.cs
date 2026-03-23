0e563fa7e545b4d3f16b179e66e25c2f500bf803
rafarush <rgarciagonzalez59@gmail.com>
2026-03-23 01:02:58 -0400

    fix update method removed from Policy.cs (at Domain layer)

--- CleanArchTemplate.Domain/Security/Policy.cs
+++ CleanArchTemplate.Domain/Security/Policy.cs
@@ -6,14 +6,4 @@ public class Policy : BaseEntity
 {
     public required string Name { get; set; }
     public List<Role>? Roles { get; set; }
-
-
-    public void Update(string name)
-    {
-        if (string.IsNullOrWhiteSpace(name))
-            throw new ArgumentException("Name cannot be null or empty", nameof(name));
-            
-        Name = name;
-        UpdatedAt = DateTime.UtcNow;
-    }
 }
